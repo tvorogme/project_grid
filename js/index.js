b247bd0c0d5993992b1f0b635027d2cf55b64ce2
Andrey Tvorozhkov <tvorog@tvorog.me>
2018-05-04 16:51:10 +0300

    Fix filter and add multifilter feature, made add project work

--- js/index.js
+++ js/index.js
@@ -1,27 +1,7 @@
-function filter_projects_by_tag(tag) {
-    console.log(tag);
+var tags = [];
 
-    const right_box = document.getElementsByClassName("right-flex-project-box")[0];
-    const left_box = document.getElementsByClassName("left-flex-project-box")[0];
-
-
-    function mapped_function(value, index) {
-        if (index % 2 === 0) {
-            // Append to right
-            right_box.appendChild(value);
-
-        } else {
-            // Append to left
-            left_box.appendChild(value);
-        }
-    }
-
-    // Get all 'cards'
-
-    var project_elements = document.getElementsByClassName("project_box");
-
-
-    if (tag && tag !== "Сбросить фильтры") {
+function filter_projects_by_tag(tag, projects) {
+    if (tag) {
         function filter_function(element) {
             // Get all 'span' in element
             var element_tags = element.getElementsByTagName("span");
@@ -39,32 +19,85 @@ function filter_projects_by_tag(tag) {
 
         }
 
-        var filtered_elements = [];
-
-
-        for (var k = 0; k < project_elements.length; k += 1) {
-
-            if (filter_function(project_elements[k])) {
-                filtered_elements.push(project_elements[k]);
-            }
-        }
-
-
-        for (var l = 0; l < filtered_elements.length; l += 1) {
-            mapped_function(filtered_elements[l], l + 1)
-        }
-
+        return projects.filter(filter_function)
     } else {
-        // Last one is hidden text, so skip it
-        for (var i = 0; i < project_elements.length; i += 1) {
-            mapped_function(project_elements[i], i + 1);
-            project_elements[i].style.display = 'block';
+        function display_project_blok(element, index) {
+            element.style.display = 'block';
         }
+
+        projects.map(display_project_blok);
+        return projects
     }
 }
 
+function colorize_tags() {
+    var tags_menu = Array.from(document.getElementsByClassName("get_project_with_tag"));
+
+    if (tags.length > 0) {
+        tags_menu.map(function (value) {
+            if (tags.indexOf(value.innerText) !== -1) {
+                value.style.color = "#49B398"
+            }
+        })
+    } else {
+        tags_menu.map(function (value) {
+            value.style.color = '#613D8D'
+        })
+    }
+
+}
+
+function filter_by_tags(new_tag) {
+    var projects = Array.from(document.getElementsByClassName("project_box"));
+
+    // First box - add button
+    projects.splice(0, 1);
+
+    if (new_tag === "Сбросить фильтры") {
+        filter_projects_by_tag(null, projects);
+        tags = [];
+        colorize_tags();
+        return true
+    }
+
+
+    if (tags.indexOf(new_tag) === -1) {
+        tags.push(new_tag);
+        colorize_tags();
+    }
+
+    var project_elements = filter_projects_by_tag(tags[0], projects);
+
+
+    for (var i = 1; i < tags.length; i += 1) {
+        project_elements = filter_projects_by_tag(tags[i], project_elements);
+    }
+
+    const right_box = document.getElementsByClassName("right-flex-project-box")[0];
+    const left_box = document.getElementsByClassName("left-flex-project-box")[0];
+
+
+    function mapped_function(value, index) {
+        if (index % 2 === 0) {
+            // Append to right
+            right_box.appendChild(value);
+
+        } else {
+            // Append to left
+            left_box.appendChild(value);
+        }
+    }
+
+    project_elements.map(mapped_function);
+
+}
+
 window.addEventListener('load', function () {
-    var project_elements = document.getElementsByClassName("project_box");
+    var project_elements = Array.from(document.getElementsByClassName("project_box"));
+
+    // First box - add button
+    project_elements.splice(0, 1);
+
     var tags = [];
     var blacklist_tags = [];
     var blacklist_tags_blocks = document.getElementsByClassName("get_project_with_tag");
@@ -85,10 +118,7 @@ window.addEventListener('load', function () {
         }
     }
 
-
-    for (i = 0; i < project_elements.length; i += 1) {
-        get_tags_from_project(project_elements[i]);
-    }
+    project_elements.map(get_tags_from_project);
 
     var tag_menu = document.getElementsByClassName("tags-project-box-inside-wrapper")[0];
 
@@ -98,6 +128,6 @@ window.addEventListener('load', function () {
 
     document.body.onclick = function (e) {
         e = window.event ? event.srcElement : e.target;
-        if (e.className && (e.className.indexOf('project_box_tag') !== -1 || e.className.indexOf('get_project_with_tag') !== -1)) filter_projects_by_tag(e.innerText);
+        if (e.className && (e.className.indexOf('project_box_tag') !== -1 || e.className.indexOf('get_project_with_tag') !== -1)) filter_by_tags(e.innerText);
     };
 });
